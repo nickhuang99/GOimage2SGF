b9447aa5e93f3080bad5030835fb5a1b1e9825ed
nick huang <nickhuang99@hotmail.com>
2025-04-24 19:51:51 +0800

    debugging

--- utility.h
+++ utility.h
@@ -7,4 +7,6 @@
 std::pair<std::vector<double>, std::vector<double>> detectUniformGrid(const cv::Mat& image);
 std::vector<cv::Point2f> findIntersections(const std::vector<double>& horizontal_lines, const std::vector<double>& vertical_lines);
 void processGoBoard(const cv::Mat& image_bgr, cv::Mat& board_state, cv::Mat& board_with_stones);
+std::string generateSGF(const cv::Mat& board_state,  const std::vector<cv::Point2f>& intersections);
+std::string determineSGFMove(const cv::Mat& before_board_state, const cv::Mat& next_board_state);
 #endif // UTILITY_H
